d6b5b4c0ff4e0cc37d333901b8f6d4db0ff39954
Doyoung.Kim <dyflower@nate.com>
2024-03-27 16:44:42 +0900

    :sparkles: Feat : start map with current position
    지도의 시작 위치를 현재위치가 되는데, 이에 대한 UX를 개선하였습니다.

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -7,6 +7,8 @@ import CurrentPositionIcon from '../../../public/icons/currentPosition.svg';
 export default function Map() {
   const [src, setSrc] = useState<string>();
   const [currentMap, setCurrentMap] = useState<currentMapType>();
+  const [currentXY, setCurrentXY] = useState<number[]>([]);
+  const [loading, setLoading] = useState<Boolean>(true);
   const APPKEY = process.env.NEXT_PUBLIC_TMAP_API;
 
   interface currentMapType {
@@ -15,56 +17,62 @@ export default function Map() {
   }
 
   const onLoadMap = () => {
-    const mapInstance = new window.Tmapv2.Map('map_div', {
-      center: new window.Tmapv2.LatLng(37.56701114710962, 126.9973611831669),
-      width: '100%',
-      height: '90vh',
-      zoom: 15,
-      zoomControl: false,
-      scrollwheel: true,
-    });
-    setCurrentMap(mapInstance);
-    currentPosition(mapInstance);
-  };
-
-  const currentPosition = (map: currentMapType) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         let lat = position.coords.latitude;
         let lon = position.coords.longitude;
 
+        const mapInstance = new window.Tmapv2.Map('map_div', {
+          center: new window.Tmapv2.LatLng(lat, lon),
+          width: '100%',
+          height: '90vh',
+          zoom: 15,
+          zoomControl: false,
+          scrollwheel: true,
+        });
+
         //팝업 생성
         const content =
           "<div style='position: relative;'>" +
-          "<div style='font-size: 12px; line-height: 15px;'>" +
-          '현재위치' +
+          "<div style='font-size: 12px;'>" +
+          '내위치' +
           '</div>' +
           '</div>';
 
         const marker = new window.Tmapv2.Marker({
           position: new window.Tmapv2.LatLng(lat, lon),
-          map: map,
+          map: mapInstance,
         });
 
         const InfoWindow = new window.Tmapv2.InfoWindow({
           position: new window.Tmapv2.LatLng(lat, lon),
           content: content,
           type: 2,
-          map: map,
+          map: mapInstance,
         });
 
-        map.setCenter(new window.Tmapv2.LatLng(lat, lon));
-        map.setZoom(15);
+        mapInstance.setCenter(new window.Tmapv2.LatLng(lat, lon));
+        mapInstance.setZoom(15);
+        setCurrentMap(mapInstance);
+        setCurrentXY([lat, lon]);
       });
     }
+    setLoading(false);
   };
 
+  const handleCurrentButton = () => {
+    currentMap?.setCenter(new window.Tmapv2.LatLng(currentXY[0], currentXY[1]));
+    currentMap?.setZoom(15);
+  };
+
+  // 1차 스크립트
   useEffect(() => {
     if (window.Tmapv2) {
       setSrc(window.Tmapv2._getScriptLocation());
     }
-  }, [setSrc]);
+  }, [src]);
 
+  // 2차 스크립트
   useEffect(() => {
     if (src) {
       const script = document.createElement('script');
@@ -72,40 +80,41 @@ export default function Map() {
       script.async = true;
       script.onload = onLoadMap;
 
-      document.body.appendChild(script);
+      //이전 스크립트 제거
+      const prevScript = document.querySelector('script[src^="' + src + '"]');
+      if (prevScript) {
+        document.body.removeChild(prevScript);
+      }
 
-      // return () => {
-      //   // 페이지 이동 시 지도 인스턴스 정리
-      //   if (mapInstance) {
-      //     mapInstance.destroy();
-      //   }
-      // };
+      document.body.appendChild(script);
     }
   }, [src]);
 
   return (
     <>
-      {/* <script
-        type='text/javascript'
-        src={`https://apis.openapi.sk.com/tmap/jsv2?version=1&appkey=${APPKEY}`}
-      /> */}
       <Script
         type='text/javascript'
         src={`https://apis.openapi.sk.com/tmap/jsv2?version=1&appkey=${APPKEY}`}
         strategy='beforeInteractive'
       />
       {src && <Script src={`${src}tmapjs2.min.js?version=20231206`} />}
-      <div id='map_div' className='relative'>
-        <button
-          onClick={() => {
-            currentMap ? currentPosition(currentMap) : null;
-          }}
-          className='absolute top-2 left-2 z-10'
-        >
-          <CurrentPositionIcon />
-          <p className='text-xs'>내위치</p>
-        </button>
-      </div>
+      {!loading ? (
+        <div id='map_div' className='relative'>
+          <button
+            onClick={() => {
+              handleCurrentButton();
+            }}
+            className='absolute top-2 left-2 z-10'
+          >
+            <div className='hover:bg-gray-300 rounded-full'>
+              <CurrentPositionIcon />
+            </div>
+            <p className='text-xs'>내위치</p>
+          </button>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </>
   );
 }
