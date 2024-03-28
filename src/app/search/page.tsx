311d8445a9533cff9b15a2cbd35f81abbaeb6217
Doyoung.Kim <dyflower@nate.com>
2024-03-28 15:51:11 +0900

    :lipstick: Design : search page
    팝업 스토어 검색 페이지의 기본 디자인입니다.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -12,25 +12,18 @@ export default function SearchList() {
   };
   const num = 5;
   console.log(category);
-  
+
   return (
     <div className='h-[100vh]'>
-      <SearchBar onSearching={false} />
+      <SearchBar searchBarStyle={'date'} />
       <ToggleCategory setCategory={handleCategory} />
       <div className='flex text-sm m-3 mt-5 -mb-3 text-gray-500'>
         검색결과 <p className='font-bold text-black ml-1'>{num}개</p>의 팝업스토어가 있습니다.
       </div>
       <div className='h-full mt-4 pb-48 overflow-auto'>
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
-        <PopupCard />
+        <PopupCard icon='heart' /> <PopupCard icon='heart' /> <PopupCard icon='heart' />
+        <PopupCard icon='heart' /> <PopupCard icon='heart' /> <PopupCard icon='heart' />
+        <PopupCard icon='heart' /> <PopupCard icon='heart' /> <PopupCard icon='heart' />
       </div>
     </div>
   );
