98a76e0abb3b7fdf79cc21655fca5292ca181dad
Doyoung.Kim <dyflower@nate.com>
2024-03-28 15:49:03 +0900

    :lipstick: Design : main page design
    메인페이지의 기본적인 디자인입니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
+'use client';
+
 import PopupCard from '@/components/card/PopupCard';
 import Map from '@/components/map/Map';
 import SearchBar from '@/components/searchbar/SearchBar';
 import BottomSlide from '@/components/slide/BottomSlide';
+import { destinationState } from '@/store/destination';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
 
 export default function Home() {
+  const [searchStyle, setSearchStyle] = useState('circle');
+  const [destination, setDestinationState] = useRecoilState(destinationState);
+
+  useEffect(() => {
+    if (destination.length !== 0) {
+      setSearchStyle('bar');
+    } else {
+      setSearchStyle('circle');
+    }
+  }, [destination]);
   return (
     <div>
-      <SearchBar onSearching={true} />
+      <SearchBar searchBarStyle={searchStyle} />
       <Map />
-      <BottomSlide content={<PopupCard />} />
+      <BottomSlide content={<PopupCard icon='heart' />} />
     </div>
   );
 }
