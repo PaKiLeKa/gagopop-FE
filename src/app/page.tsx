4025ef787a06d415b2a074edbd24e96e1c2dc788
Doyoung.Kim <dyflower@nate.com>
2024-03-30 19:38:08 +0900

    :sparkles: Feat : main page
    메인페이지 화면과 바텀 슬라이드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import PopupCard from '@/components/card/PopupCard';
 import Map from '@/components/map/Map';
 import SearchBar from '@/components/searchbar/SearchBar';
 import BottomSlide from '@/components/slide/BottomSlide';
-import { destinationState } from '@/store/destination';
+import { destinationState } from '@/store/search';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-
+import { api } from '@/api';
+import { PopupTypewithWish } from '../types/types';
 export default function Home() {
   const [searchStyle, setSearchStyle] = useState('circle');
+  const [popupList, setPopupList] = useState<PopupTypewithWish[]>([]);
   const [destination, setDestinationState] = useRecoilState(destinationState);
 
   useEffect(() => {
@@ -19,11 +21,38 @@ export default function Home() {
       setSearchStyle('circle');
     }
   }, [destination]);
+
+  useEffect(() => {
+    api
+      .get('/popup/find-all-with-wish')
+      .then((res) => {
+        setPopupList(res.data);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }, []);
+
   return (
     <div>
       <SearchBar searchBarStyle={searchStyle} />
       <Map />
-      <BottomSlide content={<PopupCard icon='heart' />} />
+      <BottomSlide
+        content={
+          popupList.length > 0 && (
+            <div className='h-full mt-4 pb-48 overflow-auto'>
+              {popupList?.map((popup) => (
+                <PopupCard
+                  key={popup.popupStore.id}
+                  icon={popup.inWishlist === true ? 'heart' : 'emptyheart'}
+                  info={popup.popupStore}
+                  period={['open', 'opensoon', 'endsoon', 'end']}
+                />
+              ))}
+            </div>
+          )
+        }
+      />
     </div>
   );
 }
