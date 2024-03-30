2caf82a112175afc0738806ae2c96d089425261c
Doyoung.Kim <dyflower@nate.com>
2024-03-30 20:42:32 +0900

    :sparkles: Feat : usePeriod Hook
    팝업의 오픈여부를 확인해주는 커스텀훅

---
+++ src/hooks/usePeriod.ts
@@ -0,0 +1,30 @@
+import { PopupType } from '@/types/types';
+import { useEffect, useState } from 'react';
+
+const usePeriod = (popup: PopupType) => {
+  const [periodState, setPeriodState] = useState<string>('');
+  const [diffDay, setDiffDay] = useState<number>(0);
+
+  useEffect(() => {
+    const today = new Date().getTime();
+    const startDate = new Date(popup?.startDate).getTime();
+    const endDate = new Date(popup?.endDate).getTime();
+    
+    if ((startDate <= today && today <= endDate) || (startDate <= today && startDate === endDate)) {
+      setPeriodState('open');
+      if (Math.floor((endDate - today) / (1000 * 60 * 60 * 24)) < 8) {
+        setPeriodState('endsoon');
+        setDiffDay(Math.floor((endDate - today) / (1000 * 60 * 60 * 24)));
+      }
+    } else {
+      if (Math.floor((today - endDate) / (1000 * 60 * 60 * 24)) > 0) {
+        setPeriodState('end');
+      } else {
+        setPeriodState('opensoon');
+        setDiffDay(Math.floor((startDate - today) / (1000 * 60 * 60 * 24)));
+      }
+    }
+  }, []);
+
+  return { periodState, diffDay };
+};
