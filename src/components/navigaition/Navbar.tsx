8bf6221f2fe88854adfa0e1abbe609ab6853b71d
Doyoung.Kim <dyflower@nate.com>
2024-03-30 19:38:56 +0900

    :lipstick: Design : navbar top border
    네비게이션 바 버튼 상단 검은 바 추가

--- src/components/navigaition/Navbar.tsx
+++ src/components/navigaition/Navbar.tsx
@@ -2,6 +2,8 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 import HomeIcon from '../../../public/icons/navbar/home.png';
 import PopupIcon from '../../../public/icons/navbar/popup.png';
 import WishIcon from '../../../public/icons/navbar/wish.png';
@@ -11,39 +13,60 @@ import PopupHover from '../../../public/icons/navbar/popup_h.png';
 import WishHover from '../../../public/icons/navbar/wish_h.png';
 import ProfileHover from '../../../public/icons/navbar/profile_h.png';
 
-import Image from 'next/image';
-
 export default function Navbar() {
-  const [isLogin, setIsLogIn] = useState(true);
-  
+  const [isLogin, setIsLogIn] = useState(false);
+  const pathname = usePathname();
+  const initailPathName = pathname?.split('/')[1];
+
   return (
-    <div className='absolute bottom-0 flex justify-around items-center w-full h-16 bg-white border-t-2 border-gray-400 z-20'>
-      <Link href={'/'} className='flex flex-col justify-center items-center group'>
+    <div className='absolute bottom-0 flex just items-center w-full h-16 p-4 bg-white border-t border-black z-20 shadow-[0_-10px_10px_-10px_rgba(0,0,0,0.4)]'>
+      <Link href={'/'} className='flex flex-col justify-center items-center w-[84px] group '>
         <Image src={HomeIcon} alt='홈으로 이동' className='group-hover:hidden' />
         <Image src={HomeHover} alt='홈으로 이동' className='hidden group-hover:block' />
         <p className='text-xs text-gray-400 group-hover:text-black'>홈</p>
+        {initailPathName === '' ? (
+          <div className='absolute top-0 bg-black w-[80px] h-1 rounded-[0_0_80px_80px]'></div>
+        ) : null}
       </Link>
-      <Link href={'/popup'} className='flex flex-col justify-center items-center group'>
+      <Link href={'/popup'} className='flex flex-col justify-center items-center w-[84px] group'>
         <Image src={PopupIcon} alt='팝업정보로 이동' className='group-hover:hidden' />
         <Image src={PopupHover} alt='팝업정보로 이동' className='hidden group-hover:block' />
         <p className='text-xs text-gray-400'>팝업정보</p>
+        {initailPathName === 'popup' ? (
+          <div className='absolute top-0 bg-black w-[80px] h-1 rounded-[0_0_80px_80px]'></div>
+        ) : null}
       </Link>
-      <Link href={'/wishlist'} className='flex flex-col justify-center items-center group'>
+      <Link
+        href={'/wishlist/id'}
+        className='flex flex-col justify-center items-center w-[84px] group'
+      >
         <Image src={WishIcon} alt='위시리스트으로 이동' className='group-hover:hidden' />
         <Image src={WishHover} alt='위시리스트으로 이동' className='hidden group-hover:block' />
         <p className='text-xs text-gray-400 group-hover:text-black'>위시리스트</p>
+        {initailPathName === 'wishlist' ? (
+          <div className='absolute top-0 bg-black w-[80px] h-1 rounded-[0_0_80px_80px]'></div>
+        ) : null}
       </Link>
       {isLogin ? (
-        <Link href={'/profile/id'} className='flex flex-col justify-center items-center group'>
+        <Link
+          href={'/profile/id'}
+          className='flex flex-col justify-center items-center w-[84px] group'
+        >
           <Image src={ProfileIcon} alt='프로필로 이동' className='group-hover:hidden' />
           <Image src={ProfileHover} alt='프로필로 이동' className='hidden group-hover:block' />
           <p className='text-xs text-gray-400 group-hover:text-black'>내정보</p>
+          {initailPathName === 'profile' || initailPathName === 'cs-center' ? (
+            <div className='absolute top-0 bg-black w-[80px] h-1 rounded-[0_0_80px_80px]'></div>
+          ) : null}
         </Link>
       ) : (
-        <Link href={'/login'} className='flex flex-col justify-center items-center group'>
-          <Image src={ProfileIcon} alt='로그인으로 이동' className='group-hover:hidden' />
-          <Image src={ProfileHover} alt='로그인으로 이동' className='hidden group-hover:block' />
+        <Link href={'/login'} className='flex flex-col justify-center items-center w-[84px] group'>
+          <Image src={ProfileIcon} alt='프로필로 이동' className='group-hover:hidden' />
+          <Image src={ProfileHover} alt='프로필로 이동' className='hidden group-hover:block' />
           <p className='text-xs text-gray-400 group-hover:text-black'>로그인</p>
+          {initailPathName === 'profile' || initailPathName === 'cs-center' ? (
+            <div className='absolute top-0 bg-black w-[80px] h-1 rounded-[0_0_80px_80px]'></div>
+          ) : null}
         </Link>
       )}
     </div>
