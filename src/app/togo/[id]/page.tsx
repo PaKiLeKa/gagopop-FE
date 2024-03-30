db87e75c185cc77357d806fada3966754dee1810
Doyoung.Kim <dyflower@nate.com>
2024-03-30 19:46:18 +0900

    :sparkles: Feat : add find direction
    도보경로 찾기 프로토타입

---
+++ src/app/togo/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function TogoList() {
+  return <div>hi!</div>;
+}
--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -5,13 +5,31 @@ import { useEffect, useState } from 'react';
 import CurrentPositionIcon from '../../../public/icons/currentPosition.svg';
 import Splash from '../splash/Splash';
 import Testicon from '../../../public/icons/marker/startpin.svg';
+import { PopupTypewithWish } from '@/types/types';
+import { api } from '@/api';
+import axios from 'axios';
+import { useRecoilState } from 'recoil';
+import { destinationState } from '@/store/search';
 
 export default function Map() {
   const [src, setSrc] = useState<string>();
   const [currentMap, setCurrentMap] = useState<currentMapType>();
   const [currentXY, setCurrentXY] = useState<number[]>([]);
   const [loading, setLoading] = useState<Boolean>(true);
+  const [destination, setDestinationState] = useRecoilState(destinationState);
+  const [destinationXY, setDeestinationXY] = useState<string>('');
   const APPKEY = process.env.NEXT_PUBLIC_TMAP_API;
+
+  const [popupList, setPopupList] = useState<PopupTypewithWish[]>();
+
+  useEffect(() => {
+    const destinationLatLng: string[] = [];
+    destination?.map((v) => {
+      destinationLatLng.push(v.longitude + ',' + v.latitude);
+    });
+    setDeestinationXY(destinationLatLng.join('_'));
+    console.log(destinationLatLng.join(','));
+  }, [destination]);
 
   interface currentMapType {
     setCenter: (arg0: any) => void;
@@ -27,7 +45,7 @@ export default function Map() {
         const mapInstance = new window.Tmapv2.Map('map_div', {
           center: new window.Tmapv2.LatLng(lat, lon),
           width: '100%',
-          height: '90vh',
+          height: '83vh',
           zoom: 15,
           zoomControl: false,
           scrollwheel: true,
@@ -84,6 +102,200 @@ export default function Map() {
     }
   }, [src]);
 
+  useEffect(() => {
+    api
+      .get('/popup/find-all-with-wish')
+      .then((res) => {
+        setPopupList(res.data);
+        console.log(res.data);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }, []);
+
+  useEffect(() => {
+    // 팝업 리스트가 있고 지도 객체가 로드되었을 때 실행되는 콜백 함수
+    const handlePopupMarkers = () => {
+      if (popupList && currentMap) {
+        popupList.forEach((popup, index) => {
+          const { latitude, longitude, name } = popup.popupStore; // 팝업 객체에서 위도, 경도, 이름 추출
+
+          // 마커 생성
+          const marker = new window.Tmapv2.Marker({
+            position: new window.Tmapv2.LatLng(latitude, longitude),
+            map: currentMap,
+          });
+
+          // 마커에 번호 붙이기
+          marker.index = index;
+
+          // 마커 클릭 시 정보 창 열기
+          marker.addListener('click', () => {
+            // 정보 창 내용 설정
+            const infoWindowContent = `
+              <div>
+                <h3>${name}</h3>
+              </div>
+            `;
+
+            // 정보 창 생성
+            const infoWindow = new window.Tmapv2.InfoWindow({
+              position: new window.Tmapv2.LatLng(latitude, longitude),
+              type: 2,
+              content: infoWindowContent,
+              map: currentMap,
+            });
+
+            // 정보 창에 번호 붙이기
+            infoWindow.index = index;
+          });
+        });
+      }
+    };
+
+    // 팝업 리스트나 지도 객체가 변경될 때마다 실행
+    handlePopupMarkers();
+  }, [popupList, currentMap]);
+
+  const handleFindPath = () => {
+    const startLat = currentXY[0];
+    const endLng = currentXY[1];
+    api
+      .get(`/popup/find-route?latitude=${startLat}&longitude=${endLng}&pid=3,4,5`)
+      .then((res) => res.data)
+      .then((res) => {
+        const marker_s = new window.Tmapv2.Marker({
+          position: new window.Tmapv2.LatLng(37.564991, 126.983937),
+          icon: 'https://via.placeholder.com/50',
+          iconSize: new window.Tmapv2.Size(24, 38),
+          map: currentMap,
+        });
+        const marker_e = new window.Tmapv2.Marker({
+          position: new window.Tmapv2.LatLng(37.566158, 126.98894),
+          icon: 'https://via.placeholder.com/50',
+          iconSize: new window.Tmapv2.Size(24, 38),
+          map: currentMap,
+        });
+
+        const handleShowPath = async () => {
+          const startLat = currentXY[0];
+          const endLng = currentXY[1];
+          try {
+            const response = await axios.post(
+              'https://apis.openapi.sk.com/tmap/routes/pedestrian',
+              {
+                startX: '126.983937',
+                startY: '37.564991',
+                endX: '126.988940',
+                endY: '37.566158',
+                reqCoordType: 'WGS84GEO',
+                resCoordType: 'EPSG3857',
+                startName: '출발지',
+                endName: '도착지',
+                passList: destinationXY,
+              },
+              {
+                headers: {
+                  appKey: APPKEY,
+                  'Content-Type': 'application/json',
+                },
+              },
+            );
+
+            const resultData = response.data.features;
+
+            // 결과 출력
+            const tDistance =
+              '총 거리: ' + (resultData[0].properties.totalDistance / 1000).toFixed(1) + 'km, ';
+            const tTime = '총 시간: ' + (resultData[0].properties.totalTime / 60).toFixed(0) + '분';
+
+            console.log(tDistance + tTime);
+
+            // 기존 그려진 라인 & 마커 초기화
+            // if (resultdrawArr.length > 0) {
+            // for (const marker of resultdrawArr) {
+            // marker.setMap(null);
+            // }
+            const resultdrawArr = [];
+            // }
+
+            const drawInfoArr = [];
+
+            for (const result of resultData) {
+              const geometry = result.geometry;
+              const properties = result.properties;
+
+              if (geometry.type === 'LineString') {
+                for (const coord of geometry.coordinates) {
+                  const latlng = new window.Tmapv2.Point(coord[0], coord[1]);
+                  const convertPoint = new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(
+                    latlng,
+                  );
+                  const convertChange = new window.Tmapv2.LatLng(
+                    convertPoint._lat,
+                    convertPoint._lng,
+                  );
+                  drawInfoArr.push(convertChange);
+                }
+              } else {
+                let markerImg = '';
+                let size;
+                var pType = '';
+
+                if (properties.pointType === 'S') {
+                  markerImg = 'https://via.placeholder.com/50';
+                  size = new window.Tmapv2.Size(24, 38);
+                } else if (properties.pointType === 'E') {
+                  markerImg = 'https://via.placeholder.com/50';
+                  size = new window.Tmapv2.Size(24, 38);
+                  pType = 'E';
+                } else {
+                  markerImg = 'https://via.placeholder.com/50';
+                  size = new window.Tmapv2.Size(8, 8);
+                  pType = 'P';
+                }
+
+                const latlon = new window.Tmapv2.Point(
+                  geometry.coordinates[0],
+                  geometry.coordinates[1],
+                );
+                const convertPoint = new window.Tmapv2.Projection.convertEPSG3857ToWGS84GEO(latlon);
+
+                const marker = new window.Tmapv2.Marker({
+                  position: new window.Tmapv2.LatLng(convertPoint._lat, convertPoint._lng),
+                  icon: markerImg,
+                  iconSize: size,
+                  map: currentMap,
+                });
+
+                resultdrawArr.push(marker);
+              }
+            }
+
+            drawLine(drawInfoArr); // drawLine 함수 호출
+          } catch (error) {
+            console.error('Error:', error);
+          }
+        };
+        handleShowPath();
+        const resultdrawArr = [];
+
+        const drawLine = (arrPoint: any[]) => {
+          let polyline;
+
+          polyline = new window.Tmapv2.Polyline({
+            path: arrPoint,
+            strokeColor: '#FCC32E',
+            strokeWeight: 6,
+            map: currentMap,
+          });
+
+          resultdrawArr.push(polyline);
+        };
+      });
+  };
+
   return (
     <>
       <Script
@@ -104,6 +316,13 @@ export default function Map() {
               <CurrentPositionIcon />
             </div>
             <p className='text-xs'>내위치</p>
+          </button>
+          <button
+            onClick={() => {
+              handleFindPath();
+            }}
+          >
+            경로탐색
           </button>
         </div>
       ) : (
