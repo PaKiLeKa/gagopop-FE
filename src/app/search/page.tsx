7c274e9cf59058af05746597ee654ce4520d2127
Doyoung.Kim <dyflower@nate.com>
2024-03-30 19:45:50 +0900

    :sparkles: Feat : search page
    팝업 검색 페이지

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,29 +1,53 @@
 'use client';
 
+import { api } from '@/api';
 import PopupCard from '@/components/card/PopupCard';
 import SearchBar from '@/components/searchbar/SearchBar';
 import ToggleCategory from '@/components/toggle/ToggleCategory';
-import { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { PopupType } from '../../types/types';
 
 export default function SearchList() {
-  const [category, setCategory] = useState<string[]>([]);
-  const handleCategory = (categoryState: string[]) => {
-    setCategory(categoryState);
-  };
-  const num = 5;
-  console.log(category);
+  const [period, setPeriod] = useState<string[]>([]);
+  const [searchList, setSearchList] = useState<PopupType[]>([]);
+  const searchParams = useSearchParams();
+  const search = searchParams?.get('search');
+  const date = searchParams?.get('date');
 
+  const handleCategory = (categoryState: string[]) => {
+    setPeriod(categoryState);
+  };
+
+  useEffect(() => {
+    api
+      .get(`/popup/find?name=${search}&date=${date}`)
+      .then((res) => {
+        setSearchList(res.data);
+      })
+      .catch(() => {
+        console.log('error');
+      });
+  }, []);
+
+  console.log(period);
   return (
     <div className='h-[100vh]'>
       <SearchBar searchBarStyle={'date'} />
       <ToggleCategory setCategory={handleCategory} />
       <div className='flex text-sm m-3 mt-5 -mb-3 text-gray-500'>
-        검색결과 <p className='font-bold text-black ml-1'>{num}개</p>의 팝업스토어가 있습니다.
+        검색결과 <p className='font-bold text-black ml-1'>{searchList?.length}개</p>의 팝업스토어가
+        있습니다.
       </div>
       <div className='h-full mt-4 pb-48 overflow-auto'>
-        <PopupCard icon='heart' /> <PopupCard icon='heart' /> <PopupCard icon='heart' />
-        <PopupCard icon='heart' /> <PopupCard icon='heart' /> <PopupCard icon='heart' />
-        <PopupCard icon='heart' /> <PopupCard icon='heart' /> <PopupCard icon='heart' />
+        {searchList?.map((popup, _i) => (
+          <PopupCard
+            key={popup.id}
+            icon={'heart'}
+            info={popup}
+            period={period}
+          />
+        ))}
       </div>
     </div>
   );
