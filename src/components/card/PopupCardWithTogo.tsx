453eedf1a9a76f0ac15c65974e5014d61f305e16
Doyoung.Kim <dyflower@nate.com>
2024-03-31 13:46:21 +0900

    :sparkles: Feat : add popupcard for diff with response
    api 응답 차이에 따른 팝업카드 생성

--- src/components/card/PopupCardWithTogo.tsx
+++ src/components/card/PopupCardWithTogo.tsx
@@ -1,47 +1,28 @@
 'use client';
 
 import Image from 'next/image';
-import Heart from '../../../public/icons/heart.svg';
-import EmptyHeart from '../../../public/icons/emptyheart.svg';
+import Togo from '../../../public/icons/togo.svg';
+import EmptyTogo from '../../../public/icons/emptytogo.svg';
 import DestinationIcon from '../../../public/icons/marker/destination_s.svg';
 import { useRecoilState } from 'recoil';
 import { destinationState } from '@/store/search';
 import Badge from '../badge/Badge';
-import { PopupType, PopupTypewithTogo, PopupTypewithWish } from '@/types/types';
+import { PopupType, PopupTypewithTogo } from '@/types/types';
+import Test from '../../../public/images/dummy.png';
 import { useRouter } from 'next/navigation';
 import usePeriod from '@/hooks/usePeriod';
 import { api, apiCred } from '@/api';
-import { useEffect, useState } from 'react';
 
-export default function PopupCard({ info, period }: { info: PopupType; period: string[] }) {
-  const [wishList, setWishList] = useState<PopupTypewithTogo[]>();
+export default function PopupCard({ info, period }: { info: PopupTypewithTogo; period: string[] }) {
   const [destination, setDestinationState] = useRecoilState(destinationState);
   const router = useRouter();
   const handleDestinationBtn = () => {
-    setDestinationState([...destination, info]);
+    setDestinationState([...destination, info.popupStore]);
   };
-  const { periodState, diffDay } = usePeriod(info);
+  const { periodState, diffDay } = usePeriod(info?.popupStore);
 
-  const handdleWishButton = () => {
-    apiCred
-      .get(`/user/wishlist/add?pid=${info.id}`)
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  };
-
-  const handdleDeleteWishButton = () => {
-    apiCred
-      .get(`/user/wishlist/delete?pid=${info?.id}`)
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  };
-
-  useEffect(() => {
-    apiCred
-      .get('/user/wishlist')
-      .then((res) => setWishList(res.data))
-      .catch((error) => console.log(error));
-  }, []);
+  const handleTogoButton = () => {};
+  const handleDeleteTogoButton = () => {};
 
   return (
     <div
@@ -52,49 +33,43 @@ export default function PopupCard({ info, period }: { info: PopupType; period: s
       <div className='w-[100px] h-[100px] relative rounded-md aspect-square overflow-hidden'>
         <Image
           onClick={() => {
-            router.push(`/popup/${info?.id}`);
+            router.push(`/popup/${info?.popupStore.id}`);
           }}
-          src={info?.imageUrl}
+          src={info?.popupStore?.imageUrl ? info.popupStore?.imageUrl : Test}
           alt='설명'
           className='w-[100px] h-[100px]'
           fill
         />
         <div
           onClick={() => {
-            wishList?.find((item) => item.popupStore && item.popupStore.id === info.id)
-              ? handdleDeleteWishButton()
-              : handdleWishButton();
+            info.inTogo ? handleDeleteTogoButton() : handleTogoButton();
           }}
           className='absolute top-1 left-1 cursor-pointer'
         >
-          {wishList?.find((item) => item.popupStore && item.popupStore.id === info.id) ? (
-            <Heart width='20' height='20' viewBox='4 0 40 40' />
-          ) : (
-            <EmptyHeart />
-          )}
+          {info?.inTogo ? <Togo width='20' height='20' viewBox='4 0 40 40' /> : <EmptyTogo />}
         </div>
       </div>
       <div className='flex flex-col justify-between w-2/3 ml-2 '>
         <div className='w-full'>
           <div className='flex justify-between mb-2'>
             <p className='text-[10px] text-gray-400'>
-              {info?.startDate.toString().substring(0, 10) +
+              {info?.popupStore?.startDate.toString().substring(0, 10) +
                 ' ~ ' +
-                info?.endDate.toString().substring(0, 10)}
+                info?.popupStore?.endDate.toString().substring(0, 10)}
             </p>
             <Badge badgeState={periodState} diff={diffDay} />
           </div>
           <p
             onClick={() => {
-              router.push(`/popup/${info?.id}`);
+              router.push(`/popup/${info?.popupStore.id}`);
             }}
             className='font-bold -mt-2'
           >
-            {info?.name}
+            {info?.popupStore?.name}
           </p>
         </div>
         <div className='flex justify-between items-end'>
-          <p className='text-[10px] text-gray-400'>{info?.address}</p>
+          <p className='text-[10px] text-gray-400'>{info?.popupStore?.address}</p>
           <button
             onClick={() => {
               handleDestinationBtn();
