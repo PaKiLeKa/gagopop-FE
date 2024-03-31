a36855d01580ff70cf54587b91cf6445f8446f2c
Doyoung.Kim <dyflower@nate.com>
2024-03-31 17:14:45 +0900

    :sparkles: feat : search result update!
    검색 결과에서 전체 탭을 먼저 보여주도록 하였다. 탭의 카테고리별 갯수, 전체 검색 결과 갯수의 노출이 되도록 하였다.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,12 +5,14 @@ import PopupCard from '@/components/card/PopupCardWithWish';
 import SearchBar from '@/components/searchbar/SearchBar';
 import ToggleCategory from '@/components/toggle/ToggleCategory';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { PopupType, PopupTypewithWish } from '../../types/types';
 
 export default function SearchList() {
   const [period, setPeriod] = useState<string[]>([]);
   const [searchList, setSearchList] = useState<PopupTypewithWish[]>([]);
+  const [periodArray, setPeriodArray] = useState<string[]>([]);
+  const [total, setTotal] = useState<number>();
   const searchParams = useSearchParams();
   const search = searchParams?.get('search');
   const date = searchParams?.get('date');
@@ -18,32 +20,68 @@ export default function SearchList() {
   const handleCategory = (categoryState: string[]) => {
     setPeriod(categoryState);
   };
+  const handleSum = (total: number) => {
+    setTotal(total);
+  };
+
+  const calPeriod = (popup: PopupType) => {
+    const today = new Date().getTime();
+    const startDate = new Date(popup?.startDate).getTime();
+    const endDate = new Date(popup?.endDate).getTime();
+    let periodState;
+    let diffDay = 0;
+
+    if ((startDate <= today && today <= endDate) || (startDate <= today && startDate === endDate)) {
+      periodState = 'open';
+      if (Math.floor((endDate - today) / (1000 * 60 * 60 * 24)) < 8) {
+        periodState = 'endsoon';
+        diffDay = Math.floor((endDate - today) / (1000 * 60 * 60 * 24));
+      }
+    } else {
+      if (Math.floor((today - endDate) / (1000 * 60 * 60 * 24)) > 0) {
+        periodState = 'end';
+      } else {
+        periodState = 'opensoon';
+        diffDay = Math.floor((startDate - today) / (1000 * 60 * 60 * 24));
+      }
+    }
+    return { periodState, diffDay };
+  };
 
   useEffect(() => {
     apiCred
       .get(`/popup/find?name=${search}&date=${date}`)
       .then((res) => {
-        console.log(res.data);
         setSearchList(res.data);
+        const newPeriodArray = res.data?.map((popup: { popupStore: PopupType }) => {
+          return calPeriod(popup.popupStore).periodState;
+        });
+        setPeriodArray(newPeriodArray);
       })
       .catch(() => {
         console.log('error');
       });
   }, []);
 
+  console.log(periodArray);
   return (
-    <div className='h-[100vh]'>
-      <SearchBar searchBarStyle={'date'} />
-      <ToggleCategory setCategory={handleCategory} />
-      <div className='flex text-sm m-3 mt-5 -mb-3 text-gray-500'>
-        검색결과 <p className='font-bold text-black ml-1'>{searchList?.length}개</p>의 팝업스토어가
-        있습니다.
+    <Suspense>
+      <div className='h-[100vh]'>
+        <SearchBar searchBarStyle={'date'} />
+        <ToggleCategory
+          setCategory={handleCategory}
+          periodArray={periodArray}
+          handleSum={handleSum}
+        />
+        <div className='flex text-sm m-3 mt-5 -mb-3 text-gray-500'>
+          검색결과 <p className='font-bold text-black ml-1'>{total}개</p>의 팝업스토어가 있습니다.
+        </div>
+        <div className='h-full mt-4 pb-48 overflow-auto'>
+          {searchList?.map((popup, _i) => (
+            <PopupCard key={popup.popupStore.id} info={popup} period={period} />
+          ))}
+        </div>
       </div>
-      <div className='h-full mt-4 pb-48 overflow-auto'>
-        {searchList?.map((popup, _i) => (
-          <PopupCard key={popup.popupStore.id} info={popup} period={period} />
-        ))}
-      </div>
-    </div>
+    </Suspense>
   );
 }
--- src/components/toggle/ToggleCategory.tsx
+++ src/components/toggle/ToggleCategory.tsx
@@ -1,9 +1,27 @@
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { useState } from 'react';
 
-export default function ToggleCategory({ setCategory }: { setCategory: any }) {
-  const [value, setValue] = useState<string[]>([]);
+export default function ToggleCategory({
+  setCategory,
+  periodArray,
+  handleSum,
+}: {
+  setCategory: any;
+  periodArray: string[];
+  handleSum: any;
+}) {
+  const [value, setValue] = useState<string[]>(['open', 'opensoon', 'endsoon', 'end']);
 
+  const openCount = periodArray.filter((item) => item === 'open').length;
+  const openSoonCount = periodArray.filter((item) => item === 'opensoon').length;
+  const endSoonCount = periodArray.filter((item) => item === 'endsoon').length;
+  const endCount = periodArray.filter((item) => item === 'end').length;
+  const totalCount =
+    (value.includes('open') ? openCount : 0) +
+    (value.includes('opensoon') ? openSoonCount : 0) +
+    (value.includes('endsoon') ? endSoonCount : 0) +
+    (value.includes('end') ? endCount : 0);
+  handleSum(totalCount);
   return (
     <>
       <ToggleGroup
@@ -23,28 +41,28 @@ export default function ToggleCategory({ setCategory }: { setCategory: any }) {
           aria-label='Toggle 오픈중'
           className='h-8 rounded-full border hover:bg-green-100 hover:border-green-500 hover:text-green-500 hover:border data-[state=on]:bg-green-100 data-[state=on]:border-green-500 data-[state=on]:text-green-500 data-[state=on]:border-2'
         >
-          오픈중
+          {`오픈중 ${openCount}`}
         </ToggleGroupItem>
         <ToggleGroupItem
           value='opensoon'
           aria-label='Toggle 오픈예정'
           className='h-8 rounded-full border hover:bg-green-100 hover:border-green-500 hover:text-green-500 hover:border data-[state=on]:bg-green-100 data-[state=on]:border-green-500 data-[state=on]:text-green-500 data-[state=on]:border-2'
         >
-          오픈예정
+          {`오픈예정 ${openSoonCount}`}
         </ToggleGroupItem>
         <ToggleGroupItem
           value='end'
           aria-label='Toggle 종료'
           className='h-8 rounded-full border hover:bg-green-100 hover:border-green-500 hover:text-green-500 hover:border data-[state=on]:bg-green-100 data-[state=on]:border-green-500 data-[state=on]:text-green-500 data-[state=on]:border-2'
         >
-          종료
+          {`종료 ${endCount}`}
         </ToggleGroupItem>
         <ToggleGroupItem
           value='endsoon'
           aria-label='Toggle 오픈예정'
           className='h-8 rounded-full border hover:bg-green-100 hover:border-green-500 hover:text-green-500 hover:border data-[state=on]:bg-green-100 data-[state=on]:border-green-500 data-[state=on]:text-green-500 data-[state=on]:border-2'
         >
-          종료임박
+          {`종료임박 ${endSoonCount}`}
         </ToggleGroupItem>
       </ToggleGroup>
     </>
